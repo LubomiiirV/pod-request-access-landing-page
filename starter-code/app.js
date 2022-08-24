9777f5a2eaec94c1aac5580eae6388c753153e50
Lubomir Vaclavik <vaclaviklubomir@gmail.com>
2022-08-24 10:29:18 +0200

    added style to paragraph

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,15 +3,10 @@ const submit = document.querySelector("button");
 const para = document.createElement("p");
 para.innerHTML = "Oops! Please check your email";
 
-// const showError = () => {
-//   error.style.display = "block";
-// };
-
 submit.addEventListener("click", (e) => {
   e.preventDefault();
   if (!email.value.match(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/)) {
     return document.getElementById("newText").appendChild(para);
   }
-
   alert("Request sent, thank you.");
 });
